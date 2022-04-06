beaa511010465cbed0cf232f79dbed9b126cebfe
michael-duy <45323184+duydd25031999@users.noreply.github.com>
2022-04-06 18:40:18 +0700

    config bootstrap

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/scss/_vendor.scss";`
+      }
+    }
+  },
   devServer: {
     port: 3000
   }
